2a8eb18bae01f98ce89b37a6f2e800e2a07b5eea
mihailomitrovic <mihailommitrovic@gmail.com>
2023-04-26 12:31:44 +0200

    Sedmi commit - izmenjena pozadina div-a

---
+++ .history/funkcije/daLiImaZadatke_20230426122114.php
@@ -0,0 +1,26 @@
+<?php
+require "../konekcija/konekcija.php";
+require "../modeli/zadatak.php";
+
+$korisnik = trim($_GET["korisnik"]);
+
+$zadaci = Zadatak::vratiZadatkeZaOpcije($korisnik, $konekcija);
+
+?>
+
+<?php
+if ($zadaci != null) {
+?>
+
+<h4>Izaberite jedan od zadataka za više detalja</h4>
+<?php
+    } else {
+
+?>
+<div id="nemazadataka">
+<h4><span class="puntekst">Nemate nijedan zadatak za prikazivanje — </span></h4>
+<h4><span class="krataktekst">Nemate nijedan zadatak — </span></h4><a href="../stranice/dodaj.php" id="nemazadatakalink"><h4>unesite novi<h4></a><h4 id="uzvicnik">!</h4>
+<div>
+<?php
+    }
+?>
---
+++ .history/stranice/odjava_20230422045421.php
@@ -0,0 +1,8 @@
+<?php
+
+require("./glavna.php");
+
+session_start();
+session_destroy();
+setcookie("korisnik", "", time() - 3600);
+header("Location:index.php");
